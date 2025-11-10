7e18054e4a4c53b71542c9ad6711716ef50caa07
soumadip-dev <soumadipmajila@gmail.com>
2025-11-10 12:00:00 +0530

    feat: complete the previously unfinished implementation in auth-client.ts

--- client/lib/auth-client.ts
+++ client/lib/auth-client.ts
@@ -1,8 +1,7 @@
-// import { deviceAuthorizationClient } from 'better-auth/plugins';
+import { deviceAuthorizationClient } from 'better-auth/plugins';
 import { createAuthClient } from 'better-auth/react';
 
 export const authClient = createAuthClient({
   baseURL: process.env.NEXT_PUBLIC_AUTH_BASE_URL,
-
-  // plugins: [deviceAuthorizationClient()],
+  plugins: [deviceAuthorizationClient()],
 });
